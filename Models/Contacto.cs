b2e1ca3c07bce8fa6fceb6948ff5b361f33ea198
JESUS LOWELL QUIÑONEZ ROJO <15170790@itculiacan.edu.mx>
2020-05-17 14:49:18 -0600

    Paso 2: Creacion de la clase del modelo

---
+++ Models/Contacto.cs
@@ -0,0 +1,11 @@
+namespace Examen_Ing_Web_Lowell.Models
+{
+    public class Contacto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public int edad { get; set; }
+    }
+}
+
